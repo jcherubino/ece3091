59776bec4d4a5229089c6e1cfb99189d6a912bb5
jcherubino <josh.cherubino@gmail.com>
2021-09-04 10:56:01 +0100

    Path planner somewhat working
    - Can rotate correctly and advance to target
    - XY odometry values seem to be slightly smaller than they should

--- scripts/feature_map.py
+++ scripts/feature_map.py
@@ -7,12 +7,13 @@ This node is a publisher that publishes the feature map of targets and
 obstacles it has built using distance sensor data and odometry data. Will eventually
 integrate camera data as well
 
+Without camera data, this node doesn't need to do a whole lot.
+
 Publishes to:
     /planner/feature_map
 
 Subscribes to:
     /sensors/distances
-    /sensors/odometry_xy
 
 Written by Josh Cherubino
 Last edited 30/08/21 by Josh Cherubino
@@ -20,89 +21,43 @@ Last edited 30/08/21 by Josh Cherubino
 
 import rospy
 import math
-from ece3091.msg import DistanceData, FeatureMap, OdometryDataXY
+from ece3091.msg import DistanceData, FeatureMap
 
 RATE = 50
 
 class FeatureMapper(object):
     '''
-    Class to build a feature map from surrounding data
+    Class to build a feature map from surrounding data. Builds feature map
+    in local robot coordintes (i.e. x y system centered on front of robot
+    with rotation of robot)
     '''
-    def __init__(self, target_x=None, target_y=None):
+    def __init__(self, target_x, target_y):
         #feature map internally stored as an array of sparse coordinates
-        #the robot is always defined at the centre of the grid and coordinates
-        #are relative to this.
         #y coordinates are defined 'in front' of the robot and x coordinates are positive
         #to the right of the robot (from behind)
-        if target_x:
-            self.target_x = target_x
-            self.target_y = target_y
-        else:
-            self.target_x = []
-            self.target_y = []
+        #HOWEVER, target is in the x,y coordinate plane relative to the starting position.
+        self.target_x = target_x
+        self.target_y = target_y
         self.obstacle_x = []
         self.obstacle_y = []
-        self.cur_x = 0
-        self.cur_y = 0
-        self.cur_orientation = 0
     
-    def transform_front(self, value):
-        '''
-        Transform value from robot local coordinate space into coordinate
-        space in axes from starting position
-        Return x, y tuple
-        '''
-        theta = math.radians(self.cur_orientation)
-        return (value*math.cos(theta), value*math.sin(theta))
-
-    def transform_left(self, value):
-        '''
-        Transform value from left of robot (at 90 degree angle to front)
-        into relative coordinate space (from starting position)
-        Returns x,y tuple
-        '''
-        phi = math.radians(90 - self.cur_orientation)
-        return (-value*math.cos(phi),value*math.sin(phi))
-
-    def transform_right(self, value):
-        '''
-        Transform value from right of robot (at 90 degree angle to front)
-        into relative coordinate space (from starting position)
-        Returns x,y tuple
-        '''
-        phi = math.radians(90 - self.cur_orientation)
-        return (value*math.cos(phi), -value*math.sin(phi))
-
     def distance_callback(self, distance_data):
         '''
         Callback from distance sensors
         '''
         #update obstacles map
         #assume each detected distance value is a single point in feature map
-        #must also use robot orientation to transform points into starting coordinate system
         #front
-
         #distance data must be non zero or obstacles will be added on rover
-        delta_front = self.transform_front(distance_data.front) 
-        self.obstacle_x.append(self.cur_x + delta_front[0])
-        self.obstacle_y.append(self.cur_y + delta_front[1])
+        self.obstacle_x.append(0)
+        self.obstacle_y.append(distance_data.front)
         #left
         #temporarily comment out while no left and right sensors.
-        #delta_left = self.transform_left(distance_data.left)
-        #self.obstacle_x.append(self.cur_x + delta_left[0])
-        #self.obstacle_y.append(self.cur_y + delta_left[1])
+        #self.obstacle_x.append(-distance_data.left)
+        #self.obstacle_y.append(0)
         #right
-        #delta_right = self.transform_right(distance_data.right)
-        #self.obstacle_x.append(self.cur_x + delta_right[0])
-        #self.obstacle_y.append(self.cur_y + delta_right[1])
-
-    def odom_callback(self, odometry_data):
-        '''
-        Callback from odometry updates
-        '''
-        self.cur_x = odometry_data.x
-        self.cur_y = odometry_data.y
-        self.cur_orientation = odometry_data.orientation
+        #self.obstacle_x.append(distance_data.right)
+        #self.obstacle_y.append(0)
 
     def pop(self):
         '''
@@ -110,7 +65,7 @@ class FeatureMapper(object):
         Return in order (targets, obstacles)
         '''
         msg = FeatureMap(self.target_x, self.target_y, self.obstacle_x, self.obstacle_y)
-        #N.B. for now keep targets array intact
+        #N.B. for now keep targets intact
         #can now safely reset obstacles
         self.obstacle_x = []
         self.obstacle_y = []
@@ -121,10 +76,9 @@ def feature_map_node():
     rospy.init_node('feature_map_node', anonymous=False)
 
     #create mapper with hardcoded target
-    mapper = FeatureMapper(target_x=[30], target_y=[30])
+    mapper = FeatureMapper(target_x=30, target_y=30)
 
     distance_sub = rospy.Subscriber('/sensors/distances', DistanceData, mapper.distance_callback)
-    odom_sub = rospy.Subscriber('/sensors/odometry_xy', OdometryDataXY, mapper.odom_callback)
 
     rate = rospy.Rate(RATE)
     rospy.loginfo('Starting feature map node')
--- scripts/path_planner.py
+++ scripts/path_planner.py
@@ -3,19 +3,18 @@
 '''
 ECE3091 Group 5
 
-This node is a publisher that publishes the feature map of targets and 
-obstacles it has built using distance sensor data and odometry data. Will eventually
-integrate camera data as well
+This node is a publisher outputs motor cmds by taking in its odometry data
+and the feature map built using sensor data
 
 Publishes to:
-    /planner/feature_map
+    /actuators/motor_cmds
 
 Subscribes to:
-    /sensors/distances
+    /planner/feature_map
     /sensors/odometry_xy
 
 Written by Josh Cherubino
-Last edited 30/08/21 by Josh Cherubino
+Last edited 04/09/21 by Josh Cherubino
 '''
 
 import rospy
@@ -23,25 +22,46 @@ import math
 from ece3091.msg import MotorCmd, FeatureMap, OdometryDataXY
 
 RATE = 50
+Kp = 0.75
+ANGLE_TOL = 1 #tolerate +/- 1 degree of error
+DISTANCE_TOL = 1 #tolerate +/- cm of error
+MIN_PROXIMITY = 5 #if obstacle within this radius must circumvent.
 
 class PathPlanner(object):
     '''
     Class to plan a path given a feature map and current x y coordintes and orientation.
+
+    Implements a state machine to control what type of movement the robot does
+    at a particular instant
     '''
     def __init__(self):
         self.x = 0
         self.y = 0
         self.orientation = 0
-        self.target_x = []
-        self.target_y = []
+        self.target_x = 0
+        self.target_y = 0
         self.obstacle_x = []
-        self.obstalce_y = []
+        self.obstacle_y = []
 
+        #states for state machine
         self.ALIGN = 0
         self.APPROACH = 1
         self.CIRCUMVENT = 2
-        self.ARRIVED
+        self.ARRIVED = 3
         self.state = self.ALIGN
+
+        #table to map current state to function to execute
+        self.STEP_TABLE = [self.align, self.approach, self.circumvent, self.arrived]
+
+        #motor cmds
+        self.STOP = 0
+        self.FORWARD = 1
+        self.REVERSE = 2
+        self.CW = 3
+        self.CCW = 4
+
+        self.align_target = None #target for alignment value
+        self.align_direction = None
 
     def odom_callback(self, odom_data):
         '''
@@ -54,29 +74,196 @@ class PathPlanner(object):
     def fmap_callback(self, fmap):
         '''
         Update feature map
+        N.B. targets are in 'global' coordinate space
+        and obstacles in robot coordinate space.
         '''
-        self.target_x = fmap.target_x
-        self.target_y = fmap.target_y
+        if fmap.target_x == 0 and fmap.target_y == 0:
+            self.target_x, self.target_y = None, None
+        else:
+            self.target_x = fmap.target_x
+            self.target_y = fmap.target_y
+
         self.obstacle_x = fmap.obstacle_x
         self.obstacle_y = fmap.obstacle_y
 
+    def proportional_control(self, target, current):
+        '''
+        Proportional speed control function
+        that clips output in 0 to 1 range
+        '''
+        try:
+            error = abs(target - current)/target
+        except ZeroDivisionError:
+            return 0.0
+        #apply Kp or clip values if necessary
+        return max(0.0, min(Kp*error, 1.0))
+    
+    def check_align(self):
+        '''
+        Helper function to check alignment
+        Returns align_target, align_direction tuple
+        '''
+        align_target, _ = self.calculate_align()
+        if abs(self.orientation - align_target) > ANGLE_TOL:
+            return False
+        return True
+
+    def calculate_align(self):
+        '''
+        Helper function to calculate alignment to target
+        '''
+        delta_x = abs(self.target_x - self.x)
+        delta_y = abs(self.target_y - self.y)
+        try:
+            align_target =  math.degrees(math.atan(delta_y/delta_x))
+        except ZeroDivisionError:
+            align_target = 0
+
+        #calculate shorter rotate direction
+        theta_cw = abs((self.orientation - align_target) % 360.0)
+        theta_ccw = abs((360 - self.orientation + align_target) % 360.0)
+        #take shorter path
+        if theta_cw < theta_ccw:
+            align_direction = self.CW
+        else:
+            align_direction = self.CCW
+        return align_target, align_direction
+
+    def target_distance(self):
+        '''
+        Helper function to calculate distance to target
+        '''
+        return math.sqrt((self.x - self.target_x)**2 + (self.y - self.target_y)**2)
+
+    def align(self):
+        '''
+        Action to take in ALIGN state
+        '''
+        #N.B. for now align doesn't care what is to either side as there is 
+        #only 1 distance sensor connected.
+        #attempt to align with current target using rotate commands
+        
+        #no target 
+        if self.target_x is None:
+            #for now just do nothing.
+            #clean up align state vars
+            #self.align_target = None
+            #self.align_direction = None
+            #self.state = self.ARRIVED
+            #rospy.loginfo('No target. Moving to ARRIVED state')
+            return MotorCmd(self.STOP, 0)
+
+        if self.align_target is None:
+            #calculate alignment
+            self.align_target, self.align_direction = self.calculate_align()
+            rospy.loginfo('Move {} degrees in {} direction to align.'.format(self.align_target,
+                self.align_direction))
+
+        #once aligned within tolerance advance to APPROACH state
+        if abs(self.align_target - self.orientation) < ANGLE_TOL:
+            #clean up align state vars
+            self.align_target = None
+            self.align_direction = None
+            rospy.loginfo('Aligned. Moving to APPROACH state')
+            self.state = self.APPROACH
+            #do nothing this iteration
+            return MotorCmd(self.STOP, 0)
+
+        #otherwise must rotate
+        speed = self.proportional_control(self.align_target, self.orientation)
+        return MotorCmd(self.align_direction, speed)
+
+    def check_arrived(self):
+        '''
+        Helper method to see if we have arrived at our destination
+        Returns True if arrived and False otherwise
+        '''
+        if abs(self.x - self.target_x) <= DISTANCE_TOL and \
+                abs(self.y - self.target_y) <= DISTANCE_TOL:
+            return True
+        return False
+
+    def check_overshoot(self):
+        '''
+        Helper to see if we have overshot target
+        '''
+        EPS = 0.001
+        #if we have not overshot the target, then if we move by epsilon
+        #in current direction then error should reduce.
+        cur_error = self.target_distance()
+
+        #move by epsilon forward along current path
+        eps_x = EPS*math.cos(math.radians(self.orientation)) + self.x
+        eps_y = EPS*math.sin(math.radians(self.orientation)) + self.y
+        eps_error = math.sqrt((eps_x - self.target_x)**2 + (eps_y - self.target_y)**2)
+
+        if eps_error > cur_error:
+            return True
+        return False
+
+    def approach(self):
+        '''
+        Action to take in APPROACH state
+        Note this doesn't account for overshoot
+        '''
+        #check if we've arrived
+        if self.check_arrived():
+            rospy.loginfo('Arrived.')
+            self.state = self.ARRIVED
+            return MotorCmd(self.STOP, 0)
+        
+        #check if overshot target
+        if self.check_overshoot():
+            rospy.logwarn('Target overshot. Re-aligning')
+            self.state = self.ALIGN
+            return MotorCmd(self.STOP, 0)
+
+        #check if collision imminent. only need to check in front of us.
+        if self.obstacle_y is not None:
+            for obstacle in self.obstacle_y:
+                if obstacle < MIN_PROXIMITY:
+                    rospy.logwarn('Collision imminent. Moving to circumvent')
+                    self.state = self.CIRCUMVENT
+                    return MotorCmd(self.STOP, 0) 
+
+        #check if alignment is off.
+        if self.check_align() == False:
+            #if we have lost alignment, re-align
+            rospy.logwarn('Alignment off. Re-aligning')
+            self.state = self.ALIGN
+            return MotorCmd(self.STOP, 0)
+
+        #else move in straight line.
+        speed_x = self.proportional_control(self.target_x, self.x)
+        speed_y = self.proportional_control(self.target_y, self.y)
+
+        return MotorCmd(self.FORWARD, math.sqrt(speed_x**2 + speed_y**2))
+        
+    def circumvent(self):
+        '''
+        Action to take in CIRCUMVENT state
+        '''
+        #move away from obstacle (in direction that will take robot closer to target given robot orientation)
+        #Once certain distance travelled, move state back to ALIGN to re-align with target
+        return MotorCmd(self.STOP, 0)
+
+    def arrived(self):
+        '''
+        Action to take in arrived state
+        '''
+        #if self.target_x is not None:
+            #new target mve to align
+            #self.state = self.ALIGN
+            #rospy.loginfo('Obtained target. Moving to align')
+        return MotorCmd(self.STOP, 0)
+
     def step(self):
         '''
-        Given the current feature map and position of the robot, take a step in
-        the best direction
+        Given current state and available data, execute one 'step' in appropriate
+        direction
         '''
-        #attempt to align with target in state ALIGN
-        #Once aligned (within tolerance) move state to APPROACH
-
-        #if this will cause a collision, set state to CIRCUMVENT and 
-        #move away from obstacle (in direction that will take robot closer to target given robot orientation)
-        #Once certain distance travelled, move state back to ALIGN
-
-        #In approach state, 
-        #approach target in straight line. If we are no longer pointing towards target
-        #move back to ALIGN state
-
-        #if target reached, then move to state ARRIVED
+        #defer to state callbacks to handle determining next step
+        return self.STEP_TABLE[self.state]()
 
 def path_planner_node():
     pub = rospy.Publisher('/actuators/motor_cmds', MotorCmd, queue_size=2)
@@ -97,7 +284,7 @@ def path_planner_node():
 
 if __name__ == '__main__':
     try:
-        feature_map_node()
+        path_planner_node()
     except rospy.ROSInterruptException:
         pass
 
