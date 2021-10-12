03026ca5c1e0b071b7a4448f352683568daf0f58
jcherubino <josh.cherubino@gmail.com>
2021-10-12 10:53:50 +0100

    Cleanup files, fix obstacle corners in frame process
    Frame process corners were being detected in different fashion to
    previously believed. Also added in missing bitwise not to prevent
    detection of entire box as contour

--- scripts/frame_process.py
+++ scripts/frame_process.py
@@ -17,7 +17,7 @@ Subscribes to:
 Written by Josh Cherubino
 Last edited 20/09/21 by Josh Cherubino
 '''
-
+import os
 import rospy
 import math
 from ece3091.msg import CamData, Obstacles, Targets
@@ -26,19 +26,19 @@ import cv2 as cv
 
 obstacles = Obstacles()
 targets = Targets()
-output_features = False
-
 MIN_OBSTACLE_CONTOUR_AREA = 10000 #minimum pixel area that contour must exceed to be considered obstacle
 OBSTACLE_BLUR_KERNEL_SZ = 31 #size of medianblur kernel for obstacle detection
-RATE = 20
-HEIGHT = None
-WIDTH = None
+RATE = rospy.get_param('picam/framerate')
+HEIGHT = rospy.get_param('picam/height')
+WIDTH = rospy.get_param('picam/width')
+output_features = True
+feature_img = np.zeros((HEIGHT, WIDTH, 3), dtype=np.uint8)
 CAM_HEIGHT = 16.9 #height of camera above ground
 CAM_ANGLE_X = 62.2 #degrees
 CAM_ANGLE_Y = 48.8 #degrees
 
 def frame_callback(cam_data):
-    global obstacles, targets
+    global obstacles, targets, feature_img
     
     #convert raw data into cv object (which is actually just numpy array)
     #data is already in BGR order.
@@ -48,6 +48,7 @@ def frame_callback(cam_data):
         feature_img = img.copy()
 
     #update obstacles and targets
+    img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     obstacles.lx, obstacles.ly, obstacles.rx, obstacles.ry = detect_obstacles(img)
     targets.x, targets.y = detect_targets(img)
 
@@ -56,8 +57,9 @@ def detect_obstacles(img):
     blurred = cv.medianBlur(img, OBSTACLE_BLUR_KERNEL_SZ) #smooth image to only 'see' big objects 
     #apply OTSU thresholding to find large foreground objects
     _, thresholded = cv.threshold(blurred, 0, 255, cv.THRESH_BINARY|cv.THRESH_OTSU)
+    thresholded = cv.bitwise_not(thresholded)
     #find contours. N.B. function modifies input thresholded image
-    contours, _ = cv.findContours(thresholded, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+    _,contours, _ = cv.findContours(thresholded, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
 
     lx = []
     ly = []
@@ -71,33 +73,37 @@ def detect_obstacles(img):
             rect = cv.minAreaRect(contour)
             box = cv.boxPoints(rect)
             #store points
-            lx.append(box[0][0]),ly.append(box[0][1])
-            rx.append(box[3][0]),ry.append(box[3][1])
+            lx.append(box[1][0]),ly.append(box[1][1])
+            rx.append(box[0][0]),ry.append(box[0][1])
 
             #draw obstacle bounding box in red
             if output_features:
                 global feature_img
+                box = np.int0(box)
                 cv.drawContours(feature_img,[box],0,(0,0,255),2)  
 
-    lx, ly = pixel_to_relative_coords(lx, ly)
-    rx, ry = pixel_to_relative_coords(rx, ry)
+    #lx, ly = pixel_to_relative_coords(lx, ly)
+    #rx, ry = pixel_to_relative_coords(rx, ry)
     return lx, ly, rx, ry
 
 def detect_targets(img):
     global output_features
     #convert to greyscale for hough circles
-    greyscale = cvt.cvtColor(img, cv.COLOR_BGR2GRAY)
-    circles = cv.HoughCircles(greyscale, cv.HOUGH_GRADIENT, 1, 25, param1=65,
+    circles = cv.HoughCircles(img, cv.HOUGH_GRADIENT, 1, 25, param1=65,
             param2=37.5, minRadius=10, maxRadius=150)
     #extract circle centers
-    x, y = circles[0, :, 0].tolist(), circles[0, :, 1].tolist()
+    if circles is not None:
+        x, y = circles[0, :, 0].tolist(), circles[0, :, 1].tolist()
+    else:
+        x, y, = [], []
 
     #Draw outer circle in green
-    if output_features:
+    if output_features and circles is not None:
         global feature_img
         for i in circles[0, :]:
-            cv.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
-    return pixel_to_relative_coords(x, y)
+            cv.circle(feature_img,(i[0],i[1]),i[2],(0,255,0),2)
+    #return pixel_to_relative_coords(x, y)
+    return x,y
 
 def pixel_to_relative_coords(x, y):
     #convert list of x,y points in pixel-space to coordinates in 
@@ -112,26 +118,25 @@ def frame_process_node():
     obstacle_pub = rospy.Publisher('/planner/obstacles', Obstacles, queue_size=2)
     target_pub = rospy.Publisher('/planner/targets', Targets, queue_size=2)
 
-    #turn on feature viewing
-    output_features = rospy.get_param('/planner/show_feature_view', False)
-    if output_features == True:
-        feature_pub = rospy.Publisher('/planner/feature_view', CamData, queue_size=2)
-
     rospy.init_node('frame_process_node', anonymous=False)
-
-    HEIGHT = rospy.get_param('picam/height')
-    WIDTH = rospy.get_param('picam/width')
 
     cam_sub = rospy.Subscriber('/sensors/picam', CamData, frame_callback)
 
     rate = rospy.Rate(RATE)
     rospy.loginfo('Starting frame process node')
     
+    frame_count = 0
+
     while not rospy.is_shutdown():
         obstacle_pub.publish(obstacles)
         target_pub.publish(targets)
         if output_features:
-            feature_pub.publish(feature_img.flatten())
+            if frame_count > 0 and frame_count % 25 == 0:
+                #save frame for debugging
+               cv.circle(feature_img,(10,10),10,(255,0,0),5)
+               cv.circle(feature_img,(630,470),10,(0,0,255),5)
+               cv.imwrite('/home/pi/processed_frames/{}.bmp'.format(frame_count), feature_img)
+            frame_count += 1
         rate.sleep()
 
 if __name__ == '__main__':
